876db5c9073c0513a1ee92687339a475aa3b6c11
Skyler Espinoza <Skyler.Espinoza@sckans.edu>
2021-02-05 13:47:21 -0600

    assignment 2

--- assignment_2_canvas/main.js
+++ assignment_2_canvas/main.js
@@ -4,19 +4,15 @@ let canvas = document.querySelector('canvas');
 
 let context = canvas.getContext('2d'); 
 
-context.fillRect(50, 75, 10, 10); 
+context.fillRect(25, 100, 20, 10); 
 
 context.fillStyle = 'green'; 
-context.fillRect(100, 75, 10, 10); 
+context.fillRect(125, 55, 10, 30); 
 
 document.addEventListener('click', click => {
     context.fillRect(50, 25, 30, 30); 
 }); 
 
-document.addEventListener('keydown', keydown => {
-    if(keydown.key === 'c') {
-        context.clearRect(0, 0, canvas.width, canvas.height); 
-    }
-}); 
+
 
 // 
